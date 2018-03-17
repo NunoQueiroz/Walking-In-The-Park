95aba763a5cbe69be3ea75ab4ea3bf2d8e1f6690
Luís Marques <22.luismarques@gmail.com>
2018-03-17 18:59:03 +0000

    added Server and Client

---
+++ src/org/academiadecodigo/haltistas/halflifeminus3/Client/ClientLauncher.java
@@ -0,0 +1,14 @@
+package org.academiadecodigo.haltistas.halflifeminus3.Client;
+
+public class ClientLauncher {
+
+    public static void main(String[] args) {
+
+        String hostName = "localhost";
+        int portNumber = 9000;
+
+        new Client(hostName, portNumber).start();
+
+    }
+
+}
