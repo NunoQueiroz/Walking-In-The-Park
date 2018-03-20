e4cbb27efc58754d4a9ff2c7484640675c9285ee
Luís Marques <22.luismarques@gmail.com>
2018-03-20 22:30:43 +0000

    Created frame limits

--- src/org/academiadecodigo/haltistas/halflifeminus3/BackGround/Grid.java
+++ src/org/academiadecodigo/haltistas/halflifeminus3/BackGround/Grid.java
@@ -22,6 +22,49 @@ public class Grid {
             }
 
         }
+        createTrees();
+        createFrame();
+    }
+
+    public void createFrame() {
+        int inicialColFrame = Camera.CAMERA_WIDTH / 2 - 9;
+        int finalColFrame = Camera.CAMERA_WIDTH / 2 - 8;
+        int inicialRowFrame = Camera.CAMERA_HEIGHT / 2 - 5;
+        int finalRowFrame = Camera.CAMERA_HEIGHT / 2 - 4;
+
+        for (int col = 0; col < MAX_COL; col++) {
+
+            for (int row = 0; row < MAX_ROW; row++) {
+                if ((col > inicialColFrame && col < MAX_COL - finalColFrame) &&
+                        (row > inicialRowFrame && row < MAX_ROW - finalRowFrame)) {
+                    continue;
+                }
+
+                grid[col][row] = new UncollidableBackground(col, row, "assets/wall_tile.png");
+            }
+        }
+
+
+    }
+
+    public void createTrees() {
+        int inicalColTrees = Camera.CAMERA_WIDTH / 2 -2;
+        int finalColTrees = Camera.CAMERA_WIDTH / 2 - 1;
+        int inicialRowTrees = Camera.CAMERA_HEIGHT / 2 - 2;
+        int finalRowFTrees = Camera.CAMERA_HEIGHT / 2 - 1;
+
+        for (int col = 0; col < MAX_COL; col++) {
+            for (int row = 0; row < MAX_ROW; row++) {
+
+                if ((col > inicalColTrees && col < MAX_COL - finalColTrees) &&
+                        (row > inicialRowTrees && row < MAX_ROW - finalRowFTrees)) {
+                    continue;
+                }
+                grid[col][row] = new UncollidableBackground(col, row, "assets/tree1.png");
+
+            }
+
+        }
     }
 
     public GameBackground[][] getGrid() {
